552b0c62f4ed1ebe9332743a0d18c3966cd53c44
Chevalier88 <199614297+Chevalier3388@users.noreply.github.com>
2026-07-24 17:14:56 +0300

    Add user service

---
+++ app/services/user.py
@@ -0,0 +1,53 @@
+from uuid import UUID
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.core.exceptions import UserAlreadyExistsError
+from app.core.security import hash_password
+from app.models.user import User
+from app.repositories.user import UserRepository
+from app.schemas.user import UserCreate
+
+
+class UserService:
+    """Сервис работы с пользователями."""
+
+    def __init__(
+        self,
+        session: AsyncSession,
+    ):
+        self.repository = UserRepository(session)
+
+
+    async def create_user(
+        self,
+        data: UserCreate,
+    ) -> User:
+        """Создает нового пользователя."""
+
+        existing_user = await self.repository.get_by_email(
+            data.email
+        )
+
+        if existing_user:
+            raise UserAlreadyExistsError(
+                "Пользователь с таким адресом электронной почты уже существует"
+            )
+
+        user = User(
+            email=data.email,
+            password_hash=hash_password(
+                data.password
+            ),
+        )
+
+        return await self.repository.create(user)
+
+
+    async def get_user(
+        self,
+        user_id: UUID,
+    ) -> User | None:
+        """Возвращает пользователя по ID."""
+
+        return await self.repository.get_by_id(user_id)
